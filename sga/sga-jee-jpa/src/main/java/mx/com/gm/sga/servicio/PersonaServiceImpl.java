2ed06ff362fb0d8fb276e66b3c9c5c1c51a217e6
Carlos Villanueva <ceva_19@hotmail.com>
2018-07-20 21:57:19 -0300

    EJB Application Using JPA

---
+++ sga/sga-jee-jpa/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.gm.sga.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import mx.com.gm.sga.domain.Persona;
+
+/**
+ * Componente EJB que implementa la interface PersonaServiceRemote y PersonaServiceLocal
+ * @author PC
+ */
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaServiceLocal{
+
+    @Override
+    public List<Persona> listarPersona() {
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "Juan", "Perez", "Suarez", "jperez@gmail.com", "55668798"));
+        personas.add(new Persona(2, "Martha", "Suarez", "Jimenez", "msuarez@gmail.com", "566998811"));
+        return personas;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        
+    }
+    
+}
---
+++ sga/sga-jee-jpa/src/test/java/test/PersonaServiceTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.List;
+import javax.ejb.embeddable.EJBContainer;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.servicio.PersonaServiceLocal;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ * CLIENTE LOCAL EN EL MISMO SERVIDOR
+ * Clase que hace una llamada local (Cliente y EJB dentro del mismo servidor) a la interface PersonaServiceLocal
+ * @author PC
+ */
+public class PersonaServiceTest {
+    
+    private PersonaServiceLocal personaServiceLocal;
+    
+    @Before
+    public void setUp() throws Exception
+    {
+        /*Generamos una instancia del contenedor*/
+        EJBContainer contenedor = EJBContainer.createEJBContainer();
+        /*buscamos nuestro ejb PersonaServiceImpl mediante nuestra interface PersonaServiceLocal*/
+        personaServiceLocal = (PersonaServiceLocal)
+                contenedor.getContext().lookup("java:global/classes/PersonaServiceImpl!mx.com.gm.sga.servicio.PersonaServiceLocal");
+    }
+    
+    @Test
+    public void testEJBPersonaService()
+    {
+        System.out.println("Iniciando test EJB PersonaService");
+        assertTrue(personaServiceLocal != null);
+        assertEquals(2, personaServiceLocal.listarPersona().size());
+        System.out.println("El no. de personas es igual a " + personaServiceLocal.listarPersona().size());
+        this.desplegarPersona(personaServiceLocal.listarPersona()); //llamamos al metodo del EJB a traves de la interface local
+        System.out.println("Fin test EJB PersonaServiceLocal");
+    }
+    
+    private void desplegarPersona(List<Persona> personas)
+    {
+        for(Persona persona : personas)
+        {
+            System.out.println(persona);
+        }
+    }
+}
