128e6bcdb50f4beb8248c872cdf0f435094fea6a
Carlos Villanueva <ceva_19@hotmail.com>
2018-08-17 23:26:11 -0300

    Trabajando con el Api Criteria de JPA

---
+++ sga/sga-jee-api-criteria/src/main/java/mx/com/gm/sga/servicio/PersonaServiceImpl.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.gm.sga.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import mx.com.gm.sga.domain.Persona;
+import mx.com.gm.sga.eis.PersonaDao;
+
+/**
+ * Componente EJB que implementa la interface PersonaServiceRemote y PersonaServiceLocal
+ * @author PC
+ */
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaServiceLocal{
+    
+    @EJB
+    private PersonaDao personaDao; //inyectamos la referencia del personaDao
+
+    @Override
+    public List<Persona> listarPersona() {
+        return personaDao.findAllPersonas();
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return personaDao.findPersonaById(persona);
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return personaDao.findPersonaByEmail(persona);
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        personaDao.insertPersona(persona);
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        personaDao.updatePersona(persona);
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        personaDao.deletePersona(persona);
+    }
+    
+}
